6f307c3081072e28386164b4b83719454895a272
icaroMendes777 <114268516+icaroMendes777@users.noreply.github.com>
2023-10-05 08:22:26 -0300

    Pequenos ajustes seo e visual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-ng-app';
+  title = 'Meu portifólio';
 
 
   constructor(private router: Router) {}
